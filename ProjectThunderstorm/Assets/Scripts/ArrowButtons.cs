69fd9564cec409c51084b3f8b364aae8481cc120
lingonsoda <iveros.markus@gmail.com>
2018-04-11 15:51:09 +0200

    Moveable arrows
    added arrows that can be draged around, still a bit buggy but i works

---
+++ ProjectThunderstorm/Assets/Scripts/ArrowButtons.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ArrowButtons : MonoBehaviour 
+,IPointerEnterHandler
+,IPointerExitHandler
+{
+
+	public GameObject arrow;
+
+	private bool buttonMarked;
+	private float x, y;
+
+	void Awake()
+	{
+		buttonMarked = false;
+	}
+
+	void Update()
+	{
+		Vector3 pos = Input.mousePosition;
+		pos.z = transform.position.z - Camera.main.transform.position.z;
+		if (buttonMarked && Input.GetMouseButtonDown(0)) 
+		{
+			Instantiate (arrow);
+			arrow.transform.position = Camera.main.ScreenToWorldPoint (pos);
+		}
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		buttonMarked = true;
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		buttonMarked = false;
+	}
+}
---
+++ ProjectThunderstorm/Assets/Scripts/ArrowMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMovement : MonoBehaviour {
+
+	//public GameObject arrow;
+
+	private bool arrowMarked;
+
+	void Awake()
+	{
+		arrowMarked = false;
+	}
+
+	void Update()
+	{
+		moveWithMouse ();
+	}
+
+	void OnMouseOver()
+	{
+		arrowMarked = true;
+	}
+
+	void OnMouseExit()
+	{
+		if (!Input.GetMouseButton (0)) 
+		{
+			arrowMarked = false;
+		}
+	}
+
+	public void moveWithMouse()
+	{
+		if (arrowMarked && Input.GetMouseButton (0)) 
+		{
+			Vector3 pos = Input.mousePosition;
+			pos.z = transform.position.z - Camera.main.transform.position.z;
+			transform.position = Camera.main.ScreenToWorldPoint (pos);
+		}
+	}
+}
