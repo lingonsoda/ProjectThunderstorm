335deb16df2ce044211a536dd63e964296aa2352
lingonsoda <iveros.markus@gmail.com>
2018-04-13 10:51:49 +0200

    working arrow movement

--- ProjectThunderstorm/Assets/Scripts/ArrowButtons.cs
+++ ProjectThunderstorm/Assets/Scripts/ArrowButtons.cs
@@ -7,45 +7,15 @@ using UnityEngine.EventSystems;
 
 public class ArrowButtons : MonoBehaviour 
 ,IPointerEnterHandler
-,IPointerExitHandler
 {
 
 	public GameObject arrow;
 
-	private bool pointerOnButton;
-	private Image img;
-
-	void Awake()
-	{
-		img = GetComponent<Image> ();
-	}
-
-	void Update()
-	{
-		if(Input.GetMouseButtonDown(0) && pointerOnButton)
-		{
-			img.raycastTarget = false;
-			Instantiate (arrow);
-			Vector3 pos = Input.mousePosition;
-			pos.z = transform.position.z - Camera.main.transform.position.z;
-			arrow.transform.position = Camera.main.ScreenToWorldPoint (pos);
-		}
-		if (Input.GetMouseButtonUp (0)) 
-		{
-			img.raycastTarget = true;
-		}
-	}
-
 	public void OnPointerEnter(PointerEventData eventData)
 	{
-		Debug.Log ("mouse on " + gameObject.name);
-		pointerOnButton = true;
-
-	}
-
-	public void OnPointerExit(PointerEventData eventData)
-	{
-		Debug.Log ("mouse exit " + gameObject.name);
-		pointerOnButton = false;
+		Instantiate (arrow);
+		Vector3 pos = Input.mousePosition;
+		pos.z = transform.position.z - Camera.main.transform.position.z;
+		arrow.transform.position = Camera.main.ScreenToWorldPoint (pos);
 	}
 }
