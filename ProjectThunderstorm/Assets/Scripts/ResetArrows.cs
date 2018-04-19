c65fe3e4c39865ed41a4c6d47262328e5aa1bee8
Olsoo <patrikahlgren@live.se>
2018-04-19 16:46:52 +0200

    fixade resetarrows
    fixade resetarrows

--- ProjectThunderstorm/Assets/Scripts/ResetArrows.cs
+++ ProjectThunderstorm/Assets/Scripts/ResetArrows.cs
@@ -17,8 +17,8 @@ public class ResetArrows : MonoBehaviour {
     void OnClick() {
         for(int i = 0; i < grandParent.childCount; i++) {
             Transform parent = grandParent.transform.GetChild(i);
-            if(parent.transform.GetChild(0) != null) {
-                Destroy(parent.transform.GetChild(0));
+            if(parent.transform.childCount > 0) {
+                Destroy(parent.transform.GetChild(0).gameObject);
             }
             Debug.Log((i+1) + " child");
         }
