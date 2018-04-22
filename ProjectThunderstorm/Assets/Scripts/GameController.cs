ca8016f20060a9eaf55a03bbb55486fc76ee4bc0
teoalagic <teoalagicenglund@hotmail.com>
2018-04-22 16:19:40 +0200

    awd

--- ProjectThunderstorm/Assets/Scripts/GameController.cs
+++ ProjectThunderstorm/Assets/Scripts/GameController.cs
@@ -1,27 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
+
 using UnityEngine.UI;
 
-public class GameController : MonoBehaviour {
+
 
 
 	
 		
 	
-=======
+
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
+
 	void Start () {
 
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
 		
 	}
->>>>>>> Lingonsoda's
+
 }
