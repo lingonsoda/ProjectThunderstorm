f970cfe0f3fba6ddc76c2a3b0d012267ae0d2485
Bhaalder <37620605+Bhaalder@users.noreply.github.com>
2018-04-25 15:21:44 +0200

    loop och if
    inte klara men en början

---
+++ ProjectThunderstorm/Assets/Scripts/LoopPanel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoopPanel : MonoBehaviour {
+
+	public int LoopCount;
+	public Text countText;
+
+	void Start(){
+		LoopCount = 1;
+	}
+
+
+	void CountAdd() {
+		LoopCount++;
+		countText.text = LoopCount.ToString();
+	}
+
+	void CountSubtract(){
+		LoopCount--;
+		countText.text = LoopCount.ToString();
+	}
+
+	public void OnClickAdd(){
+		CountAdd ();
+	}
+
+	public void OnClickSubtract(){
+		CountSubtract ();
+	}
+}
