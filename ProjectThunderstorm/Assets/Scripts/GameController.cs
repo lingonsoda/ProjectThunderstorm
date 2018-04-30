3f0e4dcc56ab96cb72612d6a485a0e9b2424a383
lingonsoda <iveros.markus@gmail.com>
2018-04-30 15:22:20 +0200

    fixed menu, play, stop and reset function

--- ProjectThunderstorm/Assets/Scripts/GameController.cs
+++ ProjectThunderstorm/Assets/Scripts/GameController.cs
@@ -1,26 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.UI;
-
-
-
-
-	
-		
-	
-
 
 public class GameController : MonoBehaviour {
 
+	public GameObject menuPanel;
+	public GameObject winPanel;
+	public Button playButton;
+	public Button stopButton;
+	public Button resetButton;
+	public Button menuButton;
+	public PlayerController playerController;
 
 	void Start () {
-
+		menuPanel.SetActive (false);
+		winPanel.SetActive (false);
+		stopButton.gameObject.SetActive (false);
 	}
-	
 
 	void Update () {
 		
+	}
+
+	public void activateMenuPanel()
+	{
+		menuPanel.SetActive (true);
+		deactivateGameButtons ();
+		playerController.pausePlayer ();
+	}
+
+	public void deactivateMenuPanel()
+	{
+		menuPanel.SetActive (false);
+		activateGameButtons ();
+		playerController.startPlayer ();
+	}
+
+	public void activateWinPanel()
+	{
+		winPanel.SetActive (true);
+		deactivateGameButtons ();
+		playerController.pausePlayer ();
+	}
+
+	public void swapPlayAndStop()
+	{
+		if (playButton.gameObject.activeSelf) {
+			stopButton.gameObject.SetActive (true);
+			playButton.gameObject.SetActive (false);
+		} else if (stopButton.gameObject.activeSelf) {
+			playButton.gameObject.SetActive (true);
+			stopButton.gameObject.SetActive (false);
+		}
+	}
+
+	private void activateGameButtons()
+	{
+		resetButton.gameObject.SetActive (true);
+		menuButton.gameObject.SetActive (true);
+		if (playerController.play) {
+			stopButton.gameObject.SetActive (true);
+			playButton.gameObject.SetActive (false);
+		} else {
+			stopButton.gameObject.SetActive (false);
+			playButton.gameObject.SetActive (true);
+		}
+	}
+
+	private void deactivateGameButtons()
+	{
+		playButton.gameObject.SetActive (false);
+		stopButton.gameObject.SetActive (false);
+		resetButton.gameObject.SetActive (false);
+		menuButton.gameObject.SetActive (false);
 	}
 }
