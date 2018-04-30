2cea7254026f275084482362b305625f23aced06
lingonsoda <iveros.markus@gmail.com>
2018-04-30 09:43:08 +0200

    fixed errors

--- ProjectThunderstorm/Assets/Scripts/ResetArrows.cs
+++ ProjectThunderstorm/Assets/Scripts/ResetArrows.cs
@@ -3,38 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ResetArrows : MonoBehaviour {
+public class ResetArrows : MonoBehaviour
+{
 
-    public Button resetButton;
-    Transform grandParent;
+	public Button resetButton;
+	Transform grandParent;
+	ArrowColorTrack arrowColorTrack;
 
-    void Start() {
-        Button rBtn = resetButton.GetComponent<Button>();
-        rBtn.onClick.AddListener(OnClick);
-        grandParent = this.transform;
-    }
+	void Start ()
+	{
+		arrowColorTrack = GetComponent<ArrowColorTrack> ();
+		Button rBtn = resetButton.GetComponent<Button> ();
+		rBtn.onClick.AddListener (OnClick);
+		grandParent = this.transform;
+	}
 
-    void OnClick() {
-<<<<<<< HEAD
-        for(int i = 0; i < grandParent.childCount; i++) {
-            Transform parent = grandParent.transform.GetChild(i);
-            if(parent.transform.childCount > 0) {
-=======
-        for (int i = 0; i < grandParent.childCount; i++) {
-            Transform parent = grandParent.transform.GetChild(i);
-            if (parent.transform.childCount > 0 && parent.transform.GetChild(0).gameObject.name.Contains("Arrow")) {
->>>>>>> master
-                Destroy(parent.transform.GetChild(0).gameObject);
-                Debug.Log("Objekt borttaget vid" + "[child " + (i) + "]");
-            }
-<<<<<<< HEAD
-            Debug.Log((i+1) + " child");
-        }
+	void OnClick ()
+	{
+		for (int i = 0; i < grandParent.childCount; i++) {
+			Transform parent = grandParent.transform.GetChild (i);
+			if (parent.transform.childCount > 0) {
+				for (i = 0; i < grandParent.childCount; i++) {
+					parent = grandParent.transform.GetChild (i);
+					if (parent.transform.childCount > 0 && parent.transform.GetChild (0).gameObject.name.Contains ("Arrow")) {
+						Destroy (parent.transform.GetChild (0).gameObject);
+						Debug.Log ("Objekt borttaget vid" + "[child " + (i) + "]");
+					}
+					Debug.Log ((i + 1) + " child");
+				}
         
-=======
-        }
-        ArrowColorTrackScript.RemoveAllColor();
-        ArrowColorTrackScript.StartPositionColorTrack();
->>>>>>> master
-    }
+			}
+			arrowColorTrack.RemoveAllColor ();
+			arrowColorTrack.StartPositionColorTrack ();
+		}
+	}
 }
