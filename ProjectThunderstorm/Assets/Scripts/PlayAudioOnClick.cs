fccef85b52be9fbd5e5c1965d89d49ef9070621f
Bhaalder <37620605+Bhaalder@users.noreply.github.com>
2018-05-02 19:31:27 +0200

    added previewbutton

---
+++ ProjectThunderstorm/Assets/Scripts/PlayAudioOnClick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioOnClick : MonoBehaviour {
+
+	private AudioSource audio;
+
+	void Start () {
+		audio = GetComponent<AudioSource>();
+	}
+	
+
+	public void OnClick(){
+
+		audio.Play();
+
+	}
+}
