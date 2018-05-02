edb45a8e8b0283efc9ed5fd1285ecb3bc463eada
Olsoo <patrikahlgren@live.se>
2018-05-02 19:17:41 +0200

    Revert "Merge branch 'master' of https://github.com/lingonsoda/ProjectThunderstorm"
    This reverts commit c5945b4caf677378fb084ee6cc5431fef53a9ee6, reversing
    changes made to b7b46b640934056dae1bd5062adba950256dfbc3.

--- ProjectThunderstorm/Assets/Scripts/QPanel/QuizPlayButton.cs
+++ ProjectThunderstorm/Assets/Scripts/QPanel/QuizPlayButton.cs
@@ -5,9 +5,9 @@ using UnityEngine.UI;
 
 public class QuizPlayButton : MonoBehaviour {
     public Button quizPlayBtn;
-    public GameObject slot1, slot2, slot3, slot4, slot5;
+    public GameObject slot1, slot2, slot3, slot4;
 
-    bool slotOne, slotTwo, slotThree, slotFour, slotFive;
+    bool slotOne, slotTwo, slotThree, slotFour;
 
 
     void Start() {
@@ -21,8 +21,7 @@ public class QuizPlayButton : MonoBehaviour {
         slotTwo = slot2.GetComponent<QuizSlotCheck>().getIsCorrectAnswer();
         slotThree = slot3.GetComponent<QuizSlotCheck>().getIsCorrectAnswer();
         slotFour = slot4.GetComponent<QuizSlotCheck>().getIsCorrectAnswer();
-        slotFive = slot5.GetComponent<QuizSlotCheck>().getIsCorrectAnswer();
-        if (slotOne && slotTwo && slotThree && slotFour && slotFive) {
+        if (slotOne && slotTwo && slotThree && slotFour) {
             Debug.Log("Rätt");
         } else { Debug.Log("Fel"); }
     }
