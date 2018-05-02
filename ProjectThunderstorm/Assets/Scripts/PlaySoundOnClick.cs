af8a4cb59e0b1aafc248a8abefed691ba0c88e68
Bhaalder <37620605+Bhaalder@users.noreply.github.com>
2018-05-02 16:27:16 +0200

    sista pushen innan hemgång

---
+++ ProjectThunderstorm/Assets/Scripts/PlaySoundOnClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnClick : MonoBehaviour {
+
+	private AudioSource audio;
+
+	void Start(){
+		audio = GetComponent<AudioSource>();
+		Debug.Log("laddar audio");
+
+
+	}
+
+	public void OnClick() {
+		audio.Play();
+		Debug.Log("Playing");
+
+	}
+
+}
