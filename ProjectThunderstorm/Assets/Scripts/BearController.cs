d110edcc9da82d669c24736f5887c2d0249473d3
lingonsoda <iveros.markus@gmail.com>
2018-05-07 16:57:56 +0200

    fixed panel opening when answer is wrong

--- ProjectThunderstorm/Assets/Scripts/BearController.cs
+++ ProjectThunderstorm/Assets/Scripts/BearController.cs
@@ -1,29 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class BearController : MonoBehaviour {
+public class BearController : MonoBehaviour
+{
 
 	public PlayerController playerController;
 	public ToggleBoxCollider toggleBoxCollider;
 	public QuizPlayButton quizPlayButton;
+	public GameObject quizPanel;
+	public Button quizXButton;
 
 	private AudioSource bearAudio;
 	private Animator bearAnimation;
 	private bool bearCompleted;
+	private bool playerAtBear;
 
-	void Start () 
+	void Start ()
 	{
+		quizXButton.onClick.AddListener (bearAnswerCheck);
 		bearAudio = GetComponent<AudioSource> ();
 		bearAnimation = GetComponent<Animator> ();
 		bearAnimation.enabled = false;
 		bearCompleted = false;
+		playerAtBear = false;
 	}
 
-	void Update()
+	void Update ()
 	{
-		if (toggleBoxCollider.playerCollision) 
-		{
+		if (toggleBoxCollider.playerCollision) {
+			toggleBoxCollider.playerCollision = false;
+			playerAtBear = true;
 			bearCollided ();
 		}
 	}
@@ -33,33 +41,45 @@ public class BearController : MonoBehaviour {
 		if (bearCompleted) {
 			return;
 		} else {
-			quizPlayButton.AnswerCheck ();
 			playerController.playerCrashed = true;
-			toggleBoxCollider.playerCollision = false;
-			playerController.pausePlayer();
-			if (quizPlayButton.correctAnswer) 
-			{
+			playerController.pausePlayer ();
+			bearAnswerCheck ();
+		}
+	}
+
+	public void bearAnswerCheck()
+	{
+		if (playerAtBear) {
+			quizPlayButton.AnswerCheck ();
+			if (quizPlayButton.correctAnswer) {
 				bearAudio.Play ();
-				StartCoroutine(waitForBearToSleep());
-			}else
-			{
+				StartCoroutine (waitForBearToSleep ());
+			} else {
 				//play bear growl
-				//open quiz panel
-				//do new answercheck
+				StartCoroutine (waitForBearGrowl ());
 			}
 		}
 	}
 
-	IEnumerator waitForBearToSleep(){
+	IEnumerator waitForBearToSleep ()
+	{
 		yield return new WaitForSeconds (10);
 		bearAnimation.enabled = true;
 		StartCoroutine (bearSleeping ());
 	}
 
-	IEnumerator bearSleeping(){
+	IEnumerator bearSleeping ()
+	{
 		yield return new WaitForSeconds (2);
 		playerController.playerCrashed = false;
-		playerController.startPlayer();
+		playerController.startPlayer ();
 		bearCompleted = true;
+		playerAtBear = false;
+	}
+
+	IEnumerator waitForBearGrowl ()
+	{
+		yield return new WaitForSeconds (2);
+		quizPanel.SetActive (true);
 	}
 }
