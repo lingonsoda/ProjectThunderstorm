51c5210c6ff0263fa79cd7f85f30882c4875cecf
Olsoo <patrikahlgren@live.se>
2018-05-09 23:14:37 +0200

    Spela upp ljudet
    Nu kan man spela upp ljuden man lagt in, allt spelar i ordning och efter loopar. Har gjrot så man inte kan interagera med någon utav noterna när man tryckt play, varken dra ut dem eller dra in nya noter. Allt ska fungera flawless, ni får gärna testa! Bana 1-3.

--- ProjectThunderstorm/Assets/Scripts/QPanel/QuizLoop.cs
+++ ProjectThunderstorm/Assets/Scripts/QPanel/QuizLoop.cs
@@ -7,12 +7,18 @@ public class QuizLoop : MonoBehaviour {
 
     public int loopTimes;
     public Button plusBtn, minusBtn;
+    public Text plusText, minusText;
     QuizSlotCheck quizSlotCheck;
+
+    void Update() {
+    }
 
     void Start() {
         quizSlotCheck = this.GetComponent<QuizSlotCheck>();
         plusBtn.onClick.AddListener(plusClick);
         minusBtn.onClick.AddListener(minusClick);
+        plusText.text = (loopTimes+1) + ">";
+        minusText.text = "<" + (loopTimes + 1);
     }
 
     public int getLoopTimes() {
@@ -20,10 +26,17 @@ public class QuizLoop : MonoBehaviour {
     }
     public void plusClick() {
         ++loopTimes;
+        plusText.text = (loopTimes + 1) + ">";
+        minusText.text = "<" + (loopTimes + 1);
         quizSlotCheck.SlotCheck();
     }
     public void minusClick() {
         --loopTimes;
+        if (loopTimes < 0) {
+            loopTimes = 0;
+        }
+        plusText.text = (loopTimes + 1) + ">";
+        minusText.text = "<" + (loopTimes + 1);
         quizSlotCheck.SlotCheck();
     }
 }
