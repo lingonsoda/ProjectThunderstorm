5063fe1ac45938ee3e379d6c2e1104507d92e103
Bhaalder <37620605+Bhaalder@users.noreply.github.com>
2018-05-29 16:41:09 +0200

    sandbox(updated to work with new camera)

---
+++ ProjectThunderstorm/Assets/Scripts/DropDownMenuOptions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class DropDownMenuOptions : MonoBehaviour {
+
+	public Sprite forestMap;
+	public Sprite cityMap;
+//	public Sprite foresTile;
+//	public Sprite cityTile;
+	public Dropdown dropdownBox;
+	public GameObject backgroundImage;
+	public Transform grandParent;
+	Transform gt;
+
+	// Use this for initialization
+	void Start () {
+		gt = grandParent.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OptionChange(){
+
+		int dv = dropdownBox.value;
+
+		if (dv == 0) {
+			backgroundImage.GetComponent<Image> ().sprite = forestMap;
+		}
+
+		if (dv == 1) {
+			backgroundImage.GetComponent<Image>().sprite = cityMap;
+		}
+		
+		Debug.Log(dv);
+
+	}
+}
